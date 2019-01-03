a23a50a617b2c6c7b2e74655b2a038c4d18741f1
Jose Gómez <jgomez@robotnik.com>
2019-01-03 16:30:35 +0100

    Comentarios interesantes que voy a borrar. Commit importante

--- src/roboteq_flw100.py
+++ src/roboteq_flw100.py
@@ -3,12 +3,16 @@
 
 # Node that publishes the data of the sensor RoboteQ FLW100
 
+import math
+from math import sin, cos, pi
+
 import rospy
 import serial
 import time
+import tf
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseWithCovariance, Point
-from geometry_msgs.msg import TwistWithCovariance, Vector3
+from geometry_msgs.msg import PoseWithCovariance, Pose, Point, Quaternion
+from geometry_msgs.msg import TwistWithCovariance, Twist, Vector3
 
 ser = serial.Serial('/dev/RoboteQ-FLW100')
 
@@ -26,82 +30,139 @@ def is_digit(n):
         return False
 
 def main():
-    pub = rospy.Publisher('flw100/raw_odom', Odometry, queue_size = 1)
-    data = Odometry()
+    odom_pub = rospy.Publisher('flw100/odom', Odometry, queue_size = 10)
+    odom_broadcaster = tf.TransformBroadcaster()
     
     # Init node
-    rospy.init_node('raw_flw100')
+    rospy.init_node('roboteq_flw100')
     rate = rospy.Rate(50) # 50hz
 
     ser.baudrate = 115200
 
+    '''
     # Cogemos el offset de x
     ser.write('?MM 1\r\n')
-    x_offset = getSerialData()
-    while not is_digit(x_offset[9:]):
+    getted_x_offset = getSerialData()
+    while not is_digit(getted_x_offset[9:]):
         ser.write('?MM 1\r\n')
-        x_offset = getSerialData()
-    x_offset = int(x_offset[9:])
+        getted_x_offset = getSerialData()
+    getted_x_offset = int(getted_x_offset[9:])
     # Cogemos el offset de y 
     ser.write('?MM 2\r\n')
-    y_offset = getSerialData()
-    while not is_digit(y_offset[9:]):
+    getted_y_offset = getSerialData()
+    while not is_digit(getted_y_offset[9:]):
         ser.write('?MM 2\r\n')
-        y_offset = getSerialData()
-    y_offset = int(y_offset[9:])
+        getted_y_offset = getSerialData()
+    getted_y_offset = int(getted_y_offset[9:])
+    '''
 
+    x = 0.0
+    y = 0.0
+    th = 0.0 # Ángulo respecto a Y
+
+    vx = 0.1 # TODO: Change this vel to the correct (inside the while)
+    vy = -0.1
+    vth = 0.0
+
+    current_time = rospy.Time.now()
+    last_time = rospy.Time.now()
     
     while not rospy.is_shutdown():
+        current_time = rospy.Time.now()
         try:
+            '''
             # Leemos x
             ser.write('?MM 1\r\n')
-            x = getSerialData()
-            while not is_digit(x[9:]):
+            getted_x = getSerialData()
+            while not is_digit(getted_x[9:]):
                 ser.write('?MM 1\r\n')
-                x = getSerialData()
-            x = (int(x[9:]) - x_offset) / 10000.0 # In m
+                getted_x = getSerialData()
+            getted_x = -(int(getted_x[9:]) - getted_x_offset) / 10000.0 # In m
             # Leemos y 
             ser.write('?MM 2\r\n')
-            y = getSerialData()
-            while not is_digit(y[9:]):
+            getted_y = getSerialData()
+            while not is_digit(getted_y[9:]):
                 ser.write('?MM 2\r\n')
-                y = getSerialData()
-            y = (int(y[9:]) - y_offset) / 10000.0 # In m
+                getted_y = getSerialData()
+            getted_y = (int(getted_y[9:]) - getted_y_offset) / 10000.0 # In m
+            '''
             # Leemos velocidad x
             ser.write('?SMM 1\r\n')
-            x_vel = getSerialData()
-            while not is_digit(x_vel[11:]):
+            getted_vx = getSerialData()
+            while not is_digit(getted_vx[11:]):
                 ser.write('?SMM 1\r\n')
-                x_vel = getSerialData()
-            x_vel = int(x_vel[11:]) / 10000.0 # In m/s
+                getted_vx = getSerialData()
+            getted_vx = -int(getted_vx[11:]) / 10000.0 # In m/s
             # Leemos velocidad y
             ser.write('?SMM 2\r\n')
-            y_vel = getSerialData()
-            while not is_digit(y_vel[11:]):
+            getted_vy = getSerialData()
+            while not is_digit(getted_vy[11:]):
                 ser.write('?SMM 2\r\n')
-                y_vel = getSerialData()
-            y_vel = int(y_vel[11:]) / 10000.0 # In m/s
+                getted_vy = getSerialData()
+            getted_vy = int(getted_vy[11:]) / 10000.0 # In m/s
+            '''# Leemos giroscopio x
+            ser.write('?RMG 1\r\n')
+            getted_x_gyro = getSerialData()
+            while not is_digit(getted_x_gyro[11:]):
+                ser.write('?RMG 1\r\n')
+                getted_x_gyro = getSerialData()
+            getted_x_gyro = int(getted_x_gyro[11:]) / 10000.0 # In m/s'''
             # Leemos calidad de imagen
             # -> Laser debería ser > 1800
             # -> LED debería ser > 1300
             '''
             ser.write('?IMQ\r\n')
-            calidad = getSerialData()
-            while not is_digit(calidad[9:]):
+            getted_calidad = getSerialData()
+            while not is_digit(getted_calidad[9:]):
                 ser.write('?IMQ\r\n')
-                calidad = getSerialData()
-            calidad = int(calidad[9:])
+                getted_calidad = getSerialData()
+            getted_calidad = int(getted_calidad[9:])
             
             # Imprimimos los valores
-            print 'X:     %-10s Y:     %-10s\n' % (x,-y)
-            print 'X_vel: %-10s Y_vel: %-10s\n' % (x_vel,-y_vel)
-            print 'Calidad: '+str(calidad)+' (debería ser > 1800)'
+            print 'X:     %-10s Y:     %-10s\n' % (getted_x,getted_y)
+            print 'Vel X: %-10s Vel Y: %-10s\n' % (getted_vx,getted_vy)
+            print 'Calidad: '+str(getted_calidad)+' (debería ser > 1800)'
             print '-----------------------------------'
             '''
-            # Publicamos datos
-            data.pose.pose.position = Point(x, -y, 0)
-            data.twist.twist.linear = Vector3(x_vel, -y_vel, 0)
-            pub.publish(data)
+
+            # compute odometry in a typical way given the velocities of the robot
+            dt = (current_time - last_time).to_sec()
+            delta_x = (getted_vx * cos(th) - getted_vy * sin(th)) * dt
+            delta_y = (getted_vx * sin(th) + getted_vy * cos(th)) * dt
+            delta_th = vth * dt
+
+            x += delta_x
+            y += delta_y
+            th += delta_th
+
+            # since all odometry is 6DOF we'll need a quaternion created from yaw
+            odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
+
+            # first, we'll publish the transform over tf
+            odom_broadcaster.sendTransform(
+                (x, y, 0.0),
+                odom_quat,
+                current_time,
+                "odom_flw100", # Link que se moverá en función al otro link
+                "rb1_base_a_odom" # Link fijo de referencia
+            )
+
+            # next, we'll publish the odometry message over ROS
+            data = Odometry()
+
+            # set the position
+            data.pose.pose.position = Point(x, y, 0)
+            
+            #data.pose.pose.orientation = odom_quat
+            
+            # set the velocity
+            data.child_frame_id = "base_link"
+            data.twist.twist = Twist(Vector3(getted_vx, getted_vy, 0), Vector3(0,0,0))
+
+            # publish the message
+            odom_pub.publish(data)
+
+            last_time = current_time
             rate.sleep()
         except KeyboardInterrupt:
             exit()
